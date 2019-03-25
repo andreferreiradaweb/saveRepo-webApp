06e3fe2d45e094e16e2ad745ce843c38f584be4c
Andre Ferreira <andrebmx789@gmail.com>
2019-03-25 03:25:50 -0300

    consumindo api, criando paginas e estilização

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './style.css'
+import './style.css';
 
 import Header from './components/header';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
